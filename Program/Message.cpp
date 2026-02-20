e37337898b4e36da6d1b885d88676ae7df725f56
hxun202 <kangsojin0918@gmail.com>
2026-02-20 21:44:18 +0900

    2026.2.20

---
+++ Program/Message.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "Message.h"
+
+void Message::Send(const char* content)
+{
+
+}
+
+void Message::Send(int frequency)
+{
+
+}
+
+void Message::Send(const char* content, int frequency)
+{
+
+}
