8f9bc3965283ec8208062fcb369adb88bc94aef6
hxun202 <kangsojin0918@gmail.com>
2026-03-05 21:43:57 +0900

    2026.3.5 포인터

---
+++ Program/Resource.cpp
@@ -0,0 +1,12 @@
+#include "Resource.h"
+#include "stdafx.h"
+
+Resource::Resource()
+{
+	cout << "Created Resource" << endl;
+}
+
+Resource::~Resource()
+{
+	cout << "Destroyed Resource" << endl;
+}
