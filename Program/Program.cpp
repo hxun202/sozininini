03a6a4def2ae59fde65d286f8fcd8588b54ff321
hxun202 <kangsojin0918@gmail.com>
2026-03-11 21:43:21 +0900

    2026.3.11 반복자

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,10 +1,87 @@
 ﻿#include "stdafx.h"
+#include "unordered_set"
+#include <unordered_map>
 
 int main()
 {
 #pragma region 연관 컨테이너
 	// 특정한 방식으로 데이터를 저장하고 검색하는 컨테이너입니다.
 
+#pragma region unordered set
+
+	//unordered_set<const char*> unordered_set;
+
+	//unordered_set.max_load_factor(2.0f);
+
+	//unordered_set.insert("Doran's Ring");
+	//unordered_set.insert("Doran's Blade");
+	//unordered_set.insert("Doran's Shield");
+	//unordered_set.insert("Frozen Heart");
+	//unordered_set.insert("Sterak's Gage");
+	//unordered_set.insert("Randuin's Omen");
+	//unordered_set.insert("Knight's Vow");
+
+	// unordered_set.insert("Death of Rabadon");
+	// unordered_set.insert("Essence Reaver");
+
+	//cout << "Load Factor : " << unordered_set.load_factor() << endl;
+	//cout << "Bucket Count : " << unordered_set.bucket_count() << endl;
+
+	//if (unordered_set.find("Doran's shield") != unordered_set.end())
+	//{
+	//	cout << "the data exists..." << endl;
+	//}
+	//else
+	//{
+	//	cout << "the data does not exits..." << endl;
+	//}
+
+	//unordered_set.erase("Knight's Vow");
+
+	//for (const char* element : unordered_set)
+	//{
+	//	cout << element << endl;
+	//}
+
+#pragma endregion
+
+#pragma region unordered map
+
+	//unordered_map<std::string, int> unordered_map;
+
+	//unordered_map["Potion"] = 2;
+	//unordered_map["Elixir"] = 1;
+	//unordered_map["Antidote"] = 2;
+	//unordered_map["Oil"] = 3;
+
+	//string name;
+
+	//std::cin >> name;
+
+	//if (unordered_map[name] > 0 && unordered_map.find(name) != unordered_map.end())
+	//{
+	//	unordered_map[name]--;
+
+	//	cout << name << " use " << endl;
+
+	//	if (unordered_map[name] <= 0)
+	//	{
+	//		unordered_map.erase(name);
+
+	//		cout << name << "All the " << name << " has been used" << endl;
+	//	}
+	//}
+	//else
+	//{
+	//	cout << "Doesn't Exist." << endl;
+	//}
+
+	//for (const auto & element : unordered_map)
+	//{
+	//	cout  << "Name : " << element.first << " Quantity : " << element.second << endl;
+	//}
+
+#pragma endregion
 
 
 #pragma endregion
